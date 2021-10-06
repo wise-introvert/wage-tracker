b361aa2271993b296f3b8f33debb19a247fc3c9f
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2021-10-05 23:37:38 -0400

    feat: hook tax api with frontend

--- pages/api/tax.ts
+++ pages/api/tax.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import fetch, { Response } from "node-fetch";
 import cheerio from "cheerio";
